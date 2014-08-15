f5d47c93dd2bc97dfa752bcdf03ed66c02613f84
Tarik Onalan <phi.quantum@gmail.com>
2014-08-16 00:46:00 +0300

    It works. Sort of

--- src/markov/Markov.java
+++ src/markov/Markov.java
@@ -4,14 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 
 
 public class Markov {
     public File file;
-    public Map<Pair<String, String>, Pair<String, Integer>> chain;
+    public Map<Pair, List<String>> chain;
     
     public Markov(String fileName) throws IOException {
         file = new File(fileName);
@@ -19,59 +22,57 @@ public class Markov {
     }
     
     public static void main(String[] args) throws IOException {
-        Markov markov = new Markov("src/markov/text.txt");
-        System.out.println(markov.generate(250));
+        Markov markov = new Markov("text.txt");
+        System.out.println(markov.generate(200));
     }
     
     public void init() throws IOException {
-        chain = new HashMap<Pair<String, String>, Pair<String, Integer>>();
+        chain = new HashMap<Pair, List<String>>();
         
         BufferedReader reader = new BufferedReader(new FileReader(file));
         
         String a, b = next(reader), c = next(reader);
-        Pair<String, String> wordPair;
-        Pair<String, Integer> numPair;
+        Pair wordPair;
+        List<String> values;
         
         while(true) {
             a = b;
             b = c;
             c = next(reader);
-            
             if(c == null)
                 break;
             
-            wordPair = new Pair<>(a, b);
-            numPair = chain.get(wordPair);
-            if(numPair == null)
-                numPair = new Pair<String, Integer>(c, 0);
-            else
-                numPair.b++;
-            
-            chain.put(wordPair, numPair);
+            wordPair = new Pair(a, b);
+            values = chain.get(wordPair);
+            if(values == null) {
+                values = new ArrayList<>();
+                values.add(c);
+                chain.put(wordPair, values);
+            } else
+                values.add(c);
         }
     }
     
     public String generate(int length) {
         StringBuilder text = new StringBuilder();
-        Entry<Pair<String, String>, Pair<String, Integer>>[] values = 
-                chain.entrySet().toArray(new Entry[chain.entrySet().size()]);
+        Entry<Pair, List<String>>[] values = 
+                chain.entrySet().toArray(new Entry[chain.size()]);
         int random = (int)(Math.random() * values.length);
         String a, b;
-        Pair<String, Integer> current = null;
-        
+        List<String> current = null;
         do {
             a = values[(int)(Math.random() * values.length)].getKey().a;
             b = values[(int)(Math.random() * values.length)].getKey().b;
-            current = chain.get(new Pair<String, String>(a, b));
+            current = chain.get(new Pair(a, b));
         } while(current == null);
         
         for(int i = 0; i < length; i++) {
             text.append(b).append(" ");
             
             a = b;
-            b = current.a;
+            b = current.get((int)(Math.random() * current.size()));
             
-            current = chain.get(new Pair<String, String>(a, b));
+            current = chain.get(new Pair(a, b));
         }
         
         return text.toString();
@@ -97,13 +98,30 @@ public class Markov {
         return value;
     }
     
-    public class Pair<U, V> {
-        public U a;
-        public V b;
+    public class Pair {
+        public String a, b;
         
-        public Pair(U _a, V _b) {
+        public Pair(String _a, String _b) {
             a = _a;
             b = _b;
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            if (o==null || !(o instanceof Pair))
+                return false;
+            
+            Pair p = (Pair) o;
+            
+            return a.equals(p.a) && b.equals(p.b);
+        }
+        
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 67 * hash + Objects.hashCode(a);
+            hash = 67 * hash + Objects.hashCode(b);
+            return hash;
         }
     }
 }
